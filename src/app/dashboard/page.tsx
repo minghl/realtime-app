023688cd22def443c248973d1c038ec351c12b0e
minghl <a879307253@gmail.com>
2023-12-26 09:26:04 +0100

    init

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import React, { FC } from "react";
+
+interface IProps {};
+
+const page:FC<IProps> = ({}) => {
+    return <Button>Hello</Button>
+};
+
+export default page;
